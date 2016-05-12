3288cf470f733118918824068a28d6bee86f40be
Nathan Conley <nateconley123@gmail.com>
2016-05-11 17:34:49 -0700

    Added a readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#React WordPress
+
+This is just a project that is helping me to learn React. Don't expect this to be something that you would want to use for your own projects (although that might one day become a reality). I will be using WordPress REST API.
--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-	
-	render() {
-		var { name } = this.props;
-		return (
-			<h1>Hello, { name }!</h1>
-		);
-	}
-}
+import Layout from './components/Layout';
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout name="Nate" />, app);
+ReactDOM.render(<Layout />, app);
